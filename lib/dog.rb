c612da0d90bb00384ce2383bc800442703e2d6f1
amemonkey <ameliafunaki789@gmail.com>
2018-06-08 20:17:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,44 @@
-# dog.rb
+# # dog.rb
+# class Dog
+ 
+# def initialize(name)
+#   @name = 
+   
+#   def bark
+#     @bark = "Woof"
+#   end
+  
+#   def sit
+#     puts = "The Doge is sitting. you frackers."
+#   end
+  
+# end
+
+# puts Dog.sit 
+# #should output "Woof!"
+
+# 
+# 
+# 
+# 
+
+class Dog
+  def initialize(bark, sit)
+    @bark 
+    @sit 
+  end
+  
+  def bark
+    @bark
+    puts "Woof"
+  end
+  
+  def sit
+    @sit 
+    puts = "The Doge is sitting. you frackers."
+  end
+  
+end
+
+adele_goldberg = Person.new("adele_goldberg")
+alan_kay = Person.new("alan_kay")
